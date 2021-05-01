705df708712825aecddf966e91e704cb1273b112
Daniel marin <daniel.marin.depablo@gmail.com>
2021-05-01 19:38:02 +0200

    Estructura y enlaces creados

---
+++ js/datos.js
@@ -0,0 +1,24 @@
+const listadeTareas = new Array(
+    {
+        idtarea: 0,
+        titulo: 'Estudiar JavaScript',
+        prioridad: 'URGENTE',
+
+    },
+    {
+        idtarea: 1,
+        titulo: 'Hacer la compra',
+        prioridad: 'Mensual',
+    },
+    {
+        idtarea: 2,
+        titulo: 'Hacer la comida',
+        prioridad: 'Diaria',
+    },
+    {
+        idtarea: 3,
+        titulo: 'Clases de pádel',
+        prioridad: 'Mensual',
+    }
+)
+console.log(listadeTareas.titulo);
