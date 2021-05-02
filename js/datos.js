1662180a2517810f4115898c8ec7e47a79614af4
Daniel marin <daniel.marin.depablo@gmail.com>
2021-05-02 13:24:11 +0200

    Parte superior completada
    Se pueden añadir tareas con prioridad y cambio de color

--- js/datos.js
+++ js/datos.js
@@ -20,34 +20,39 @@ class ListadeTareas {
         let div = document.createElement('div');
         let parrf = document.createElement('p');
         let btnRemove = document.createElement('button');
+        div.classList.add(this.prioridad)
 
 
         let parrfText = document.createTextNode(this.titulo);
         parrf.appendChild(parrfText);
-        btnRemove.classList.add("btn-close");
+        btnRemove.classList.add(`btn-close`);
 
         div.appendChild(parrf);
         div.appendChild(btnRemove);
 
+
         switch (this.prioridad) {
-            case 'urgente':
-                console.log(this.prioridad);
+            case '0':
+                alert('Debe seleccionar una prioridad')
+                break;
+            case '1':
                 div.style.backgroundColor = 'red'
                 break;
-            case 'diaria':
+            case '2':
                 div.style.backgroundColor = 'lightgreen'
                 break;
-            case 'mensual':
-                div.style.backgroundColor = 'ligthblue'
+            case '3':
+                div.style.backgroundColor = 'lightblue'
                 break;
         }
 
         divPintar.appendChild(div);
 
-        btnRemove.addEventListener('click', () => {
+        btnRemove.addEventListener('click', (event) => {
 
             this.completarTarea();
             div.parentNode.removeChild(div)
+            listaDeElementos.splice(event.target, 1)
         })
 
     }
